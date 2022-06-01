cd6827e50dd453f460b44f5b911b9c98ad90e489
bodhivirapt <81915217+bodhivirapt@users.noreply.github.com>
2022-06-01 22:25:39 +0200

    Update main.js

--- 06InteractionHeadset/scripts/main.js
+++ 06InteractionHeadset/scripts/main.js
@@ -11,12 +11,12 @@ function spin(){
 setInterval(spin, 10);
 
 
-myOtherBox.addEventListener('mouseenter', function(){
+myOtherBox.addEventListener('click', function(){
 	rotationSpeed = 0.01;
 	console.log('entered');
 });
 
-myOtherBox.addEventListener('mouseleave', function(){
+myOtherBox.addEventListener('click', function(){
 	rotationSpeed = 0.001;
 	console.log('left');
 });
@@ -28,10 +28,10 @@ myOtherBox.addEventListener('mouseleave', function(){
  * click-events can use a fuse (default on mobile). So the event only fires, if the cursor is on the object for a specified time. 
  */
 
- var growspeed = 1.2;
+ var growspeed = 3;
 
  function grow(){
- 	myOtherBox.object3D.scale.x *= growspeed;
+ 	myOtherBox.object3D.rotation.x *= growspeed;
  	myOtherBox.object3D.scale.y *= growspeed;
  	myOtherBox.object3D.scale.z *= growspeed;
 	console.log(myOtherBox.object3D.scale);
